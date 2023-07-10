6bed75de6bb8bb97fe7de7d6e70681fed2d2dacf
maotterson <otterson.2@gmail.com>
2023-07-09 23:00:47 -0400

    add some error handling

--- Touch/Program.cs
+++ Touch/Program.cs
@@ -1,8 +1,22 @@
 ﻿foreach(var file in args)
 {
-    using (var createdFile = File.Create(file))
+    try
     {
-        createdFile.Close();
-        Console.WriteLine($"{createdFile.Name} created.");
+        using (var createdFile = File.Create(file))
+        {
+            createdFile.Close();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"CREATED: {createdFile.Name}");
+        }
+    }
+    catch(DirectoryNotFoundException ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"ERROR: {file} not created. Directory not found.");
+    }
+    catch
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"ERROR: {file} not created. An unexpected exception occured.");
     }
 }
