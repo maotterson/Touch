4d566667cb0785786bdce783fd6b1f15d6ba0283
maotterson <otterson.2@gmail.com>
2023-07-09 23:06:26 -0400

    reset console color when the application finishes

--- Touch/Program.cs
+++ Touch/Program.cs
@@ -1,22 +1,29 @@
-﻿foreach(var file in args)
+﻿try
 {
-    try
+    foreach (var file in args)
     {
-        using (var createdFile = File.Create(file))
+        try
         {
-            createdFile.Close();
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"CREATED: {createdFile.Name}");
+            using (var createdFile = File.Create(file))
+            {
+                createdFile.Close();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"CREATED: {createdFile.Name}");
+            }
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"ERROR: {file} not created. Directory not found.");
+        }
+        catch
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"ERROR: {file} not created. An unexpected exception occured.");
         }
     }
-    catch(DirectoryNotFoundException ex)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"ERROR: {file} not created. Directory not found.");
-    }
-    catch
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"ERROR: {file} not created. An unexpected exception occured.");
-    }
+}
+finally
+{
+    Console.ResetColor();
 }
